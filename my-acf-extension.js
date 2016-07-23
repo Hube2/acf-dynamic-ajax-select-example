ea8a40fac93c1edaa43e549f90f1338f0ca83acc
John Huebner <johnhuebner@site-seeker.com>
2016-07-23 12:23:42 -0400

    commit

---
+++ my-acf-extension.js
@@ -0,0 +1,86 @@
+	
+	jQuery(document).ready(function($){
+		// make sure acf is loaded, it should be, but just in case
+		if (typeof acf == 'undefined') { return; }
+		
+		// extend the acf.ajax object
+		var acfCustomFieldLocatioRule = acf.ajax.extend({
+			events: {
+				// this data-key must match the field key
+				// for the state field on the post page where
+				// you want to dynamically load the cities when
+				// the state is changed
+				'change [data-key="field_579376f522130"] select': '_state_change',
+				// this entry is to cause the city field to be updated
+				// when the page is loaded
+				'ready [data-key="field_579376f522130"] select': '_state_change',
+			},
+			
+			// this is our function that will perform the
+			// ajax request when the state value is changed
+			_state_change: function(e){
+				
+				// clear the city field options
+				// the data-key is the field key of the city field on post
+				var $select = $('[data-key="field_5793770922131"] select');
+				$select.empty();
+				
+				// get the state selection
+				var $value = e.$el.val();
+				
+				// a lot of the following code is copied directly 
+				// from ACF and modified for our purpose
+				
+				// I assume this tests to see if there is already a request
+				// for this and cancels it if there is
+				if( this.state_request) {
+					this.state_request.abort();
+				}
+				
+				// I don't know exactly what it does
+				// acf does it so I copied it
+				var self = this,
+						data = this.o;
+						
+				// set the ajax action that's set up in php
+				data.action = 'load_city_field_choices';
+				// set the state value to be submitted
+				data.state = $value;
+				
+				// this is another bit I'm not sure about
+				// copied from ACF
+				data.exists = [];
+				
+				// this the request is copied from ACF
+				this.state_request = $.ajax({
+					url:		acf.get('ajaxurl'),
+					data:		acf.prepare_for_ajax(data),
+					type:		'post',
+					dataType:	'json',
+					async: true,
+					success: function(json){
+						// function to update the city field choices
+						
+						// get the city field
+						var $select = $('[data-key="field_5793770922131"] select');
+						
+						// add options to the city field
+						for (i=0; i<json.length; i++) {
+							//alert(json[i]['value']+':'+json[i]['label']);
+							//var $option = $select.createElement('option');
+							//$option.value = json[i]['value'];
+							//$option.text = json[i]['label'];
+							var $value = json[i]['value'];
+							//alert($value);
+							//alert($selected.indexOf($value));
+							var $item = '<option value="'+json[i]['value']+'">'+json[i]['label']+'</option>';
+							$select.append($item);
+						}
+					}
+				});
+			},
+		});
+		
+		// triger the ready action on page load
+		$('[data-key="field_579376f522130"] select').trigger('ready');
+	});
---
+++ my-acf-extension.php
@@ -0,0 +1,137 @@
+<?php 
+	
+	new my_acf_extension();
+	
+	class my_acf_extension {
+		
+		public function __construct() {
+			// state field on city 
+			add_action('acf/load_field/key=field_579376941cecc', array($this, 'load_state_field_choices'));
+			// state field on post
+			add_action('acf/load_field/key=field_579376f522130', array($this, 'load_state_field_choices'));
+			// city field on post
+			add_action('acf/load_field/key=field_5793770922131', array($this, 'load_city_field_choices'));
+			// ajax action for loading city choices
+			add_action('wp_ajax_load_city_field_choices', array($this, 'ajax_load_city_field_choices'));
+			// enqueue js extension for acf
+			// do this when ACF in enqueuing scripts
+			add_action('acf/input/admin_enqueue_scripts', array($this, 'enqueue_script'));
+		} // end public function __construct
+		
+		public function load_state_field_choices($field) {
+			// this funciton dynamically loads the state select field choices
+			// from the state post type
+			
+			// I only want to do this on Posts and the city CPT
+			global $post;
+			if (!$post || 
+					!isset($post->ID) || 
+					(get_post_type($post->ID) != 'post' && get_post_type($post->ID) != 'city')) {
+				return $field;
+			}
+			
+			// get states
+			$args = array(
+				'post_type' => 'state',
+				'posts_per_page' => -1,
+				'orderby' => 'title',
+				'order' => 'ASC'
+			);
+			$query = new WP_Query($args);
+			$choices = array('' => '-- State --');
+			if (count($query->posts)) {
+				// populate choices
+				foreach ($query->posts as $state) {
+					$choices[$state->ID] = $state->post_title;
+				}
+				$field['choices'] = $choices;
+			}
+			return $field;
+		} // end public function load_state_field_choices
+		
+		public function load_city_field_choices($field) {
+			// this function dynamically loads city field choices
+			// based on the currently saved state
+			
+			// I only want to do this on Posts
+			global $post;
+			if (!$post || 
+					!isset($post->ID) || 
+					get_post_type($post->ID) != 'post') {
+				return $field;
+			}
+			// get the state post id
+			// I generally use get_post_meta() instead of get_field()
+			// when building functionality, but get_field() could be
+			// subsitited here
+			$state = intval(get_post_meta($post->ID, 'state', true));
+			$cities = $this->get_cities($state);
+			$field['choices'] = $cities;
+			return $field;
+		} // end public funciton load_city_field_choices
+		
+		public function enqueue_script() {
+			// enqueue acf extenstion
+			$handle = 'my-acf-extension';
+			
+			// I'm using this method to set the src because
+			// I don't know where this file will be located
+			// you should alter this to use the correct fundtions
+			// to set the src value to point to the javascript file
+			$src = '/'.str_replace(ABSPATH, '', dirname(__FILE__)).'/my-acf-extension.js';
+			// make this script dependent on acf-input
+			$depends = array('acf-input');
+			
+			wp_enqueue_script($handle, $src, $depends);
+		} // end public function enqueue_script
+		
+		public function ajax_load_city_field_choices() {
+			// this funtion is called by AJAX to load cities
+			// based on state selecteion
+			
+			// we can use the acf nonce to verify
+			if (!wp_verify_nonce($_POST['nonce'], 'acf_nonce')) {
+				die();
+			}
+			$state = 0;
+			if (isset($_POST['state'])) {
+				$state = intval($_POST['state']);
+			}
+			$cities = $this->get_cities($state);
+			$choices = array();
+			foreach ($cities as $value => $label) {
+				$choices[] = array('value' => $value, 'label' => $label);
+			}
+			echo json_encode($choices);
+			exit;
+		} // end public function ajax_load_city_field_choices
+		
+		private function get_cities($post_id) {
+			// $post_id is post ID of state post
+			// get all cities related to the state
+			$args = array(
+				'post_type' => 'city',
+				'posts_per_page' => -1,
+				'orderby' => 'title',
+				'order' => 'ASC',
+				'meta_query' => array(
+					array(
+						'key' => 'state',
+						'value' => $post_id
+					)
+				)
+			);
+			$query = new WP_Query($args);
+			$choices = array('' => '-- City --');
+			if (count($query->posts)) {
+				// populate choices
+				foreach ($query->posts as $post) {
+					$choices[$post->ID] = $post->post_title;
+				}
+			}
+			return $choices;
+		} // end private function get_cities
+		
+	} // end class my_acf_extension
+	
+?>
